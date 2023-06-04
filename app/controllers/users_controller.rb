2210b18024f37ab007a4770aea8bcdcdc6c472be
M. Baryoh <mustaphabaryoh@gmail.com>
2023-06-04 17:21:23 +0000

    v06041221

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,6 @@ class UsersController < ApplicationController
     @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
     session["user_id"] = @user["id"]
-    redirect_to "/users/#{@user["id"]}"
+    redirect_to "/places"
   end
 end
